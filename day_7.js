21043d34d9bbf6dfca2a38abfb6120a873e55c3e
Nouman Khan <nomiofficial68@gmail.com>
2024-04-26 00:07:44 +0500

    day 7 challange solved

---
+++ day_7.js
@@ -0,0 +1,19 @@
+// Q__6:  maximum number in array
+//
+/*write a function findMax that takes and array of 
+of numbers as an input and returns the maximum number
+in the array*/
+
+
+function findMax(arr){
+  // my solution
+//   arr = arr.sort((a,b)=> a-b)
+//  return   arr[arr.length -1 ]
+//  solution provided
+
+return Math.max(...arr)
+ 
+}
+
+console.log(findMax([-2,-3,-1,-3,-5,-7,-5,-3])) // -1
+
